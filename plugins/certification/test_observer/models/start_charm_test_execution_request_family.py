454084f4736e8d0f71249a239a61f78c370daad6
Matias Piipari <matias.piipari@gmail.com>
2025-02-07 18:51:05 +0200

    Removing plugins/certification/test_observer from version control

--- plugins/certification/test_observer/models/start_charm_test_execution_request_family.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class StartCharmTestExecutionRequestFamily(str, Enum):
-    CHARM = "charm"
-
-    def __str__(self) -> str:
-        return str(self.value)
--- plugins/certification/test_observer/models/start_snap_test_execution_request_family.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class StartSnapTestExecutionRequestFamily(str, Enum):
-    SNAP = "snap"
-
-    def __str__(self) -> str:
-        return str(self.value)
