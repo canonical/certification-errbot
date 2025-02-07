19cdc2039acb8f63c4790a2e5de4b407b84c01e2
Matias Piipari <matias.piipari@gmail.com>
2025-02-07 09:26:59 +0200

    Artefacts command taking shape

--- plugins/certification/certification.py
+++ plugins/certification/certification.py
@@ -1,6 +1,8 @@
 from errbot import BotPlugin, botcmd, re_botcmd
 import re
 import os
+from datetime import date
+from datetime import datetime
 
 from c3.client import AuthenticatedClient as C3Client
 from c3.api.physicalmachinesview.physicalmachinesview_list import sync_detailed as get_physicalmachinesview
@@ -8,15 +10,21 @@ from c3.models import PhysicalMachineView
 from c3.types import Response
 from c3_auth import get_access_token as get_c3_access_token
 
-from test_observer.models import ArtefactDTO
+from test_observer.models import ArtefactDTO, ArtefactStatus, UserDTO
 from test_observer.client import Client as TestObserverClient
 from test_observer.api.artefacts.get_artefacts_v1_artefacts_get import sync_detailed as get_artefacts
 
 import ssl_fix
+from mattermost_api_client import get_user_by_email
+
+import logging
+logger = logging.getLogger(__name__)
 
 c3_client_id = os.environ.get("C3_CLIENT_ID")
 c3_client_secret = os.environ.get("C3_CLIENT_SECRET")
 
+mattermost_token = os.environ.get("ERRBOT_TOKEN")
+mattermost_base_url = f"https://{os.environ.get('ERRBOT_SERVER')}/api/v4"
 
 if not c3_client_id or not c3_client_secret:
     raise Exception("C3_CLIENT_ID and C3_CLIENT_SECRET must be set")
@@ -24,17 +32,29 @@ if not c3_client_id or not c3_client_secret:
 C3_BASE_URL = 'https://certification.canonical.com'
 
 c3_access_token = get_c3_access_token(C3_BASE_URL, c3_client_id, c3_client_secret)
-c3_client = C3Client(base_url='https://certification.canonical.com', token=c3_access_token)
 
-test_observer_client = TestObserverClient(base_url='https://test-observer-api.canonical.com')
+now = datetime.now().date()
 
 class CertificationPlugin(BotPlugin):
     """
     A plugin for interacting with Certification things
     """
+    def __init__(self, bot, name):
+        super().__init__(bot, name)
+        self.user_cache = {}
+
+    def get_assignee_handle(self, email):
+        if email in self.user_cache:
+            return self.user_cache[email]
+        else:
+            assignee_handle = get_user_by_email(mattermost_token, mattermost_base_url, email)
+            self.user_cache[email] = assignee_handle
+            return assignee_handle
 
     @botcmd(split_args_with=" ", name="cid")
     def cid(self, msg, args):
+        c3_client = C3Client(base_url='https://certification.canonical.com', token=c3_access_token)
+
         # a command callable with !cid
         msg = ""
 
@@ -58,16 +78,64 @@ class CertificationPlugin(BotPlugin):
 
             return msg
 
-    @botcmd(split_args_with=None)
+    @botcmd(split_args_with=' ')
     def artefacts(self, msg, args):
-        msg = ''
+        logger.info("msg")
+        logger.info(msg.frm.username)
+        
+        logger.info("args")
+        logger.info(f"args: {args}")
+        test_observer_client = TestObserverClient(base_url='https://test-observer-api.canonical.com')
+
+        out_msg = ''
+        artefacts_by_user = {}
 
         with test_observer_client:
             r = get_artefacts(client=test_observer_client)
 
             for artefact in r.parsed:
-                msg += f"{artefact.name} | {artefact.version} | {artefact.status}\n"
-            
+                assignee = artefact.assignee
 
-        return msg
+                # if not assignee and not artefact.due_date:
+                #    continue
+
+                if assignee and assignee.launchpad_email:
+                    assignee_handle = self.get_assignee_handle(assignee.launchpad_email)["username"]
+                else:
+                    assignee_handle = "No assignee"
+
+                if args[0] == 'pending' and (artefact.status in [ArtefactStatus.APPROVED, ArtefactStatus.MARKED_AS_FAILED]):
+                    continue
+
+                if assignee_handle not in artefacts_by_user:
+                    artefacts_by_user[assignee_handle] = []
+
+                artefacts_by_user[assignee_handle].append(artefact)
+
+            sender_handle = msg.frm.username 
+            if sender_handle in artefacts_by_user:
+                user_artefacts = {sender_handle: artefacts_by_user[sender_handle]}
+            else:
+                user_artefacts = {}
+
+            for user, artefacts in user_artefacts.items():
+                artefacts.sort(key=lambda x: (x.assignee is None,
+                                               x.due_date is None,
+                                               False if x.due_date is None else x.due_date < now,
+                                               x.due_date or datetime.max.date()))
+                if user == "No assignee":
+                    out_msg += f"**{user}**\n"
+                else:
+                    out_msg += f"**@{user}**\n"
+                for artefact in artefacts:
+                    due_date_str = f" (due {artefact.due_date.strftime('%d-%m-%Y')})" if artefact.due_date else ""
+                    out_msg += f"- **{artefact.name} {artefact.version}**: {artefact.status.lower()}{due_date_str}\n"
+                out_msg += "\n"
+
+            # Sort un-assigned artefacts last
+            unassigned_artefacts = artefacts_by_user.pop("No assignee", [])
+            if unassigned_artefacts:
+                artefacts_by_user["No assignee"] = unassigned_artefacts
+
+        return out_msg
 
