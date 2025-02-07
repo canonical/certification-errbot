cd05c8751d16326551da875848df60a3aafd9d3c
Matias Piipari <matias.piipari@gmail.com>
2025-02-07 18:50:49 +0200

    Removing plugins/certification/c3 from version control

--- plugins/certification/c3/__init__.py
+++
@@ -1,8 +0,0 @@
-"""A client library for accessing Certification v2 API"""
-
-from .client import AuthenticatedClient, Client
-
-__all__ = (
-    "AuthenticatedClient",
-    "Client",
-)
--- plugins/certification/c3/models/role_enum.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class RoleEnum(str, Enum):
-    DUT = "DUT"
-    SUPPORT = "Support"
-
-    def __str__(self) -> str:
-        return str(self.value)
