20d2e19d926df311a1daaa077a83099f38bbef90
Omar Abou Selo <omar.selo@canonical.com>
2025-06-04 13:03:46 +0300

    Add ruff to dev dependencies and format files

--- plugins/certification/mattermost_api.py
+++ plugins/certification/mattermost_api.py
@@ -1,9 +1,11 @@
-import requests
 from os import environ
-from typing import TypedDict, Any, Dict
+from typing import Any, Dict, TypedDict
+
+import requests
 
 mattermost_token = environ.get("ERRBOT_TOKEN")
 mattermost_base_url = f"https://{environ.get('ERRBOT_SERVER')}/api/v4"
+
 
 class UserDetails(TypedDict):
     id: str
@@ -25,11 +27,10 @@ class UserDetails(TypedDict):
     timezone: Dict[str, str]
     disable_welcome_email: bool
 
+
 def get_user_by_email(token, base_url, email) -> UserDetails:
     url = f"{base_url}/users/email/{email}"
-    headers = {
-        'Authorization': f'Bearer {token}'
-    }
+    headers = {"Authorization": f"Bearer {token}"}
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.json()
